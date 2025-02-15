64961c7542c47d7be5ca54f321c5ce52872ceb56
Лазука Егор Александрович <ELazuka_in@inno.local>
2025-02-15 22:46:40 +0300

    fix

--- src/hooks/useIsDynamicContainer.ts
+++ src/hooks/useIsDynamicContainer.ts
@@ -1,9 +1,17 @@
 import { EntityId } from '@form-crafter/core'
+import { isNotEmpty } from '@form-crafter/utils'
+import { useStoreMap } from 'effector-react'
 
-import { useComponentMeta } from './useComponentMeta'
+import { useGeneratorContext } from '_contexts'
 
 export const useIsDynamicContainer = (id: EntityId): boolean => {
-    const meta = useComponentMeta(id)
+    const { services } = useGeneratorContext()
 
-    return meta.type === 'dynamic-container'
+    const type = useStoreMap({
+        store: services.componentsSchemasService.$schemas,
+        keys: [id],
+        fn: (data, [id]) => (isNotEmpty(data[id]) ? data[id].meta.type : null),
+    })
+
+    return type === 'dynamic-container'
 }
