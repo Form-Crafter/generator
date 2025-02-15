ad9b7340839dbdac1b94aa0fc78a1e35d3557079
Лазука Егор Александрович <ELazuka_in@inno.local>
2025-02-15 22:33:33 +0300

    fix

--- src/hooks/useIsDynamicContainer.ts
+++ src/hooks/useIsDynamicContainer.ts
@@ -1,9 +1,9 @@
-import { EntityId, rootComponentId } from '@form-crafter/core'
+import { EntityId } from '@form-crafter/core'
 
 import { useComponentMeta } from './useComponentMeta'
 
 export const useIsDynamicContainer = (id: EntityId): boolean => {
     const meta = useComponentMeta(id)
 
-    return id !== rootComponentId && meta.type === 'dynamic-container'
+    return meta.type === 'dynamic-container'
 }
